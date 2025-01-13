a613447c3a12609d5163be59ce55e28ddf3dfe4c
edgard2001 <eromanovskiy303@gmail.com>
2025-01-13 23:53:32 +0000

    add revolver with shoot animation

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
+    private Animator _animator;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _animator.SetTrigger("Shoot");
+        }
     }
 }
